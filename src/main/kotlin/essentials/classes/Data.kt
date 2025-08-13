ef1c4e8f4b53420b6d76b393b4a057f7527eeb39
Kim HyungGeun <hyunggeun.kim@nhnlink.co.kr>
2025-08-13 19:32:35 +0900

    코드 연습2

--- src/main/kotlin/essentials/classes/Data.kt
+++ src/main/kotlin/essentials/classes/Data.kt
@@ -1,5 +1,22 @@
+
 package essentials.classes.data
 
+import java.util.*
+
 fun main() {
-    // Write your code here
+    val user = User("John", "Doe");
+    println("User name: ${user.fullName}")
+}
+
+interface Named {
+    val name: String
+    val fullName: String
+}
+
+class User (
+    override val name: String,
+    val surname: String,
+) : Named {
+    override val fullName : String
+        get() = "$name $surname"
 }
